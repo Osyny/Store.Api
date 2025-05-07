c3eec795a882236758e89f5f9ec822939841bbbc
Osyny <oxana1404@gmail.com>
2025-05-07 17:51:10 +0300

    added migration 'PositionOrder'

--- Store.Core/ApplicationDbContext.cs
+++ Store.Core/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Store.Core
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<PositionOrder> PositionOrders { get; set; }
     }
 
 }
---
+++ Store.Core/Entities/PositionOrder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core.Entities
+{
+    public class PositionOrder :BaseEntity
+    {
+        public int CountProduct { get; set; }
+        [Required]
+        [Precision(12, 2)]
+        public decimal PriceProduct { get; set; }
+
+        [ForeignKey(nameof(Order))]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
